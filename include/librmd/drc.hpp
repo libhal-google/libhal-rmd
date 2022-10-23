07a1b09c6ffd09d7579e921b3a5247e039f334a9
Khalil Estell <kammcecorp@gmail.com>
2022-10-23 14:41:50 -0700

    Fixup DRC driver
    - Add unit tests
    - Utilize move_interceptor to update can route callback on move
    - Improve API naming
    - Extend API to include system control and direct feedback requests

--- include/librmd/drc.hpp
+++ include/librmd/drc.hpp
@@ -7,6 +7,7 @@
 #include <libhal/can/router.hpp>
 #include <libhal/enum.hpp>
 #include <libhal/map.hpp>
+#include <libhal/move_interceptor.hpp>
 #include <libhal/units.hpp>
 
 namespace hal::rmd {
@@ -15,9 +16,11 @@ namespace hal::rmd {
  *
  *
  */
-class drc
+class drc : public move_interceptor<drc>
 {
 public:
+  friend class move_interceptor<drc>;
+
   /// Operating baudrate of all RMD-X smart servos
   static constexpr hertz baudrate_hz = 1'000'000;
 
@@ -25,46 +28,46 @@ public:
   static constexpr float dps_per_lsb_angle = 1.0f;
 
   /// Commands that can be issued to a RMD-X motor
-  enum class read_commands : hal::byte
+  enum class read : hal::byte
   {
-    read_pid_data = 0x30,
-    read_acceleration_data_command = 0x33,
-    read_encoder_data_command = 0x90,
-    read_multi_turns_angle_command = 0x92,
-    read_single_circle_angle_command = 0x94,
-    read_motor_status_1_and_error_flag_commands = 0x9A,
-    read_motor_status_2 = 0x9C,
-    read_motor_status_3 = 0x9D,
+    pid_data = 0x30,
+    acceleration_data = 0x33,
+    encoder_data = 0x90,
+    multi_turns_angle = 0x92,
+    single_circle_angle = 0x94,
+    status_1_and_error_flags = 0x9A,
+    status_2 = 0x9C,
+    status_3 = 0x9D,
   };
 
-  /// Commands for actuation the motor
-  enum class actuation_commands : hal::byte
+  /// Commands for actuate the motor
+  enum class actuate : hal::byte
   {
-    torque_closed_loop_command = 0xA1,
-    speed_closed_loop_command = 0xA2,
-    position_closed_loop_command_1 = 0xA3,
-    position_closed_loop_command_2 = 0xA4,
-    position_closed_loop_command_3 = 0xA5,
-    position_closed_loop_command_4 = 0xA6,
+    torque = 0xA1,
+    speed = 0xA2,
+    position_1 = 0xA3,
+    position_2 = 0xA4,
+    position_3 = 0xA5,
+    position_4 = 0xA6,
   };
 
-  /// Commands for changing the settings of the motor
-  enum class settings_commands : hal::byte
+  /// Commands for updating motor configuration data
+  enum class write : hal::byte
   {
-    write_pid_to_ram_command = 0x31,
-    write_pid_to_rom_command = 0x32,
-    write_acceleration_data_to_ram_command = 0x34,
-    write_encoder_offset_command = 0x91,
-    write_current_position_to_rom_as_motor_zero_command = 0x19,
+    pid_to_ram = 0x31,
+    pid_to_rom = 0x32,
+    acceleration_data_to_ram = 0x34,
+    encoder_offset = 0x91,
+    current_position_to_rom_as_motor_zero = 0x19,
   };
 
   /// Commands for controlling the motor as a whole
-  enum class system_commands : hal::byte
+  enum class system : hal::byte
   {
-    clear_motor_error_flag_command = 0x9B,
-    motor_off_command = 0x80,
-    motor_stop_command = 0x81,
-    motor_running_command = 0x88,
+    clear_error_flag = 0x9B,
+    off = 0x80,
+    stop = 0x81,
+    running = 0x88,
   };
 
   /// Structure containing all of the forms of feedback acquired by an RMD-X
@@ -73,27 +76,27 @@ public:
   {
     /// Current flowing through the motor windings
     //  (-2048 <-> 2048 ==> -33A <-> 33A)
-    std::atomic<int16_t> raw_current;
+    int16_t raw_current{ 0 };
     /// Rotational velocity of the motor (1 degrees per second (dps)/LSB)
-    std::atomic<int16_t> raw_speed;
+    int16_t raw_speed{ 0 };
     /// Motor's supply voltage (0.1V/LSB)
-    std::atomic<int16_t> raw_volts;
+    int16_t raw_volts{ 0 };
     /// Signed 16-bit raw encoder count value of the motor
-    std::atomic<int16_t> encoder;
+    int16_t encoder{ 0 };
     /// Core temperature of the motor (1C/LSB)
-    std::atomic<int8_t> raw_motor_temperature;
+    int8_t raw_motor_temperature{ 0 };
     /// Error code indicating an over voltage protection event on from the motor
     /// winding output.
-    std::atomic<bool> over_voltage_protection_tripped;
+    bool over_voltage_protection_tripped{ false };
     /// Error code indicating an over temperature protection event on from the
     /// motor winding output.
-    std::atomic<bool> over_temperature_protection_tripped;
+    bool over_temperature_protection_tripped{ false };
 
     auto current()
     {
       static constexpr float raw_current_range = 2048.0f;
       static constexpr auto current_range = 33.0_A;
-      return hal::map(static_cast<float>(raw_current.load()),
+      return hal::map(static_cast<float>(raw_current),
                       std::make_pair(-raw_current_range, raw_current_range),
                       std::make_pair(-current_range, current_range));
     }
@@ -101,55 +104,68 @@ public:
     auto speed()
     {
       static constexpr auto velocity_per_lsb = 1.0_deg_per_sec;
-      return static_cast<float>(raw_speed.load()) * velocity_per_lsb;
+      return static_cast<float>(raw_speed) * velocity_per_lsb;
     }
 
     auto volts()
     {
       static constexpr float volts_per_lsb = 0.1f;
-      return static_cast<float>(raw_volts.load()) * volts_per_lsb;
+      return static_cast<float>(raw_volts) * volts_per_lsb;
     }
 
     auto temperature()
     {
       static constexpr float celsius_per_lsb = 1.0f;
-      return static_cast<float>(raw_motor_temperature.load()) * celsius_per_lsb;
+      return static_cast<float>(raw_motor_temperature) * celsius_per_lsb;
     }
   };
 
-  template<can::id_t device_id, int bus_number = 0>
-  static result<drc&> create(can_router& p_router, float p_gear_ratio)
+  static result<drc> create(can_router& p_router,
+                            float p_gear_ratio,
+                            can::id_t device_id)
   {
-    static drc drc_driver(p_router, p_gear_ratio, device_id);
-    HAL_CHECK(
-      p_router.on_receive(device_id, [](const can::message_t& p_message) {
-        drc_driver.response_handler(p_message);
-      }));
-    HAL_CHECK(drc_driver.system_control(system_commands::motor_off_command));
-    HAL_CHECK(
-      drc_driver.system_control(system_commands::motor_running_command));
+    drc drc_driver(p_router, p_gear_ratio, device_id);
+    HAL_CHECK(drc_driver.system_control(system::off));
+    HAL_CHECK(drc_driver.system_control(system::running));
     return drc_driver;
   }
 
+  drc(drc& p_old_self) = delete;
+  drc& operator=(drc& p_old_self) = delete;
+  drc(drc&& p_old_self) = default;
+  drc& operator=(drc&& p_old_self) = default;
+
   status velocity_control(rpm p_speed) noexcept;
   status position_control(angle p_angle, rpm speed) noexcept;
-  status request_feedback(read_commands p_read_command) noexcept;
-  status system_control(system_commands p_system_command) noexcept;
+  status feedback_request(read p_command) noexcept;
+  status system_control(system p_system_command) noexcept;
 
   const feedback_t& feedback() const noexcept
   {
     return m_feedback;
   }
 
-private:
-  void response_handler(const can::message_t& p_message) noexcept;
+  void operator()(const can::message_t& p_message) noexcept;
 
+private:
   drc(can_router& p_router, float p_gear_ratio, can::id_t p_device_id)
     : m_feedback{}
     , m_router(&p_router)
+    , m_route_item(p_router.add_message_callback(p_device_id))
     , m_gear_ratio(p_gear_ratio)
     , m_device_id(p_device_id)
   {
+    intercept(this);
+  }
+
+  /**
+   * @brief Update the callback location if this object is moved
+   *
+   * @param p_old_self - the old version of this driver
+   */
+  void intercept(drc* p_old_self)
+  {
+    p_old_self->m_route_item.get().handler = std::ref(*this);
   }
 
   can::message_t message(std::array<hal::byte, 8> p_payload) const noexcept
@@ -159,30 +175,45 @@ private:
     return message;
   }
 
-  int32_t rpm_to_drc_speed(rpm p_rpm, float dps_per_lsb);
+  result<int32_t> rpm_to_drc_speed(rpm p_rpm, float p_dps_per_lsb);
 
-  feedback_t m_feedback;
+  template<std::integral T>
+  result<T> bounds_check(std::floating_point auto p_float)
+  {
+    using float_t = decltype(p_float);
+    constexpr auto min = static_cast<float_t>(std::numeric_limits<T>::min());
+    constexpr auto max = static_cast<float_t>(std::numeric_limits<T>::max());
+
+    if (min < p_float && p_float < max) {
+      return static_cast<T>(p_float);
+    }
+
+    return new_error(std::errc::result_out_of_range);
+  }
+
+  feedback_t m_feedback{};
   can_router* m_router;
+  can_router::route_item m_route_item;
   float m_gear_ratio;
   can::id_t m_device_id;
 };
 
-inline int32_t drc::rpm_to_drc_speed(rpm p_rpm, float dps_per_lsb)
+inline result<std::int32_t> drc::rpm_to_drc_speed(rpm p_rpm,
+                                                  float p_dps_per_lsb)
 {
   static constexpr float dps_per_rpm = (1.0f / 1.0_deg_per_sec);
 
-  const float dps_float = (p_rpm * m_gear_ratio * dps_per_rpm) / dps_per_lsb;
-  const std::int32_t dps = static_cast<std::int32_t>(dps_float);
+  const float dps_float = (p_rpm * m_gear_ratio * dps_per_rpm) / p_dps_per_lsb;
 
-  return dps;
+  return bounds_check<std::int32_t>(dps_float);
 }
 
 inline status drc::velocity_control(rpm p_rpm) noexcept
 {
-  const auto speed_data = rpm_to_drc_speed(p_rpm, dps_per_lsb_speed);
+  const auto speed_data = HAL_CHECK(rpm_to_drc_speed(p_rpm, dps_per_lsb_speed));
 
   return m_router->bus().send(message({
-    value(actuation_commands::speed_closed_loop_command),
+    value(actuate::speed),
     0x00,
     0x00,
     0x00,
@@ -197,11 +228,11 @@ inline status drc::position_control(angle p_angle, rpm p_rpm) noexcept
 {
   static constexpr float deg_per_lsb = 0.01f;
   const auto angle = (p_angle * m_gear_ratio) / deg_per_lsb;
-  const auto angle_data = static_cast<std::int32_t>(angle);
-  const auto speed_data = rpm_to_drc_speed(p_rpm, dps_per_lsb_angle);
+  const auto angle_data = HAL_CHECK(bounds_check<std::int32_t>(angle));
+  const auto speed_data = HAL_CHECK(rpm_to_drc_speed(p_rpm, dps_per_lsb_angle));
 
   return m_router->bus().send(message({
-    value(actuation_commands::position_closed_loop_command_2),
+    value(actuate::position_2),
     0x00,
     static_cast<hal::byte>((speed_data >> 0) & 0xFF),
     static_cast<hal::byte>((speed_data >> 8) & 0xFF),
@@ -212,10 +243,10 @@ inline status drc::position_control(angle p_angle, rpm p_rpm) noexcept
   }));
 }
 
-inline status drc::request_feedback(read_commands p_read_command) noexcept
+inline status drc::feedback_request(read p_command) noexcept
 {
   return m_router->bus().send(message({
-    value(p_read_command),
+    value(p_command),
     0x00,
     0x00,
     0x00,
@@ -226,7 +257,7 @@ inline status drc::request_feedback(read_commands p_read_command) noexcept
   }));
 }
 
-inline status drc::system_control(system_commands p_system_command) noexcept
+inline status drc::system_control(system p_system_command) noexcept
 {
   return m_router->bus().send(message({
     value(p_system_command),
@@ -240,20 +271,20 @@ inline status drc::system_control(system_commands p_system_command) noexcept
   }));
 }
 
-inline void drc::response_handler(const can::message_t& p_message) noexcept
+inline void drc::operator()(const can::message_t& p_message) noexcept
 {
   if (p_message.length != 8 || p_message.id != m_device_id) {
     return;
   }
 
   switch (p_message.payload[0]) {
-    case value(read_commands::read_motor_status_2):
-    case value(actuation_commands::torque_closed_loop_command):
-    case value(actuation_commands::speed_closed_loop_command):
-    case value(actuation_commands::position_closed_loop_command_1):
-    case value(actuation_commands::position_closed_loop_command_2):
-    case value(actuation_commands::position_closed_loop_command_3):
-    case value(actuation_commands::position_closed_loop_command_4): {
+    case value(read::status_2):
+    case value(actuate::torque):
+    case value(actuate::speed):
+    case value(actuate::position_1):
+    case value(actuate::position_2):
+    case value(actuate::position_3):
+    case value(actuate::position_4): {
       auto& data = p_message.payload;
       m_feedback.raw_motor_temperature = data[1];
       m_feedback.raw_current = static_cast<int16_t>((data[3] << 8) | data[2]);
@@ -261,7 +292,7 @@ inline void drc::response_handler(const can::message_t& p_message) noexcept
       m_feedback.encoder = static_cast<int16_t>((data[7] << 8) | data[6]);
       break;
     }
-    case value(read_commands::read_motor_status_1_and_error_flag_commands): {
+    case value(read::status_1_and_error_flags): {
       auto& data = p_message.payload;
       m_feedback.raw_motor_temperature = static_cast<int8_t>(data[1]);
       m_feedback.raw_volts = static_cast<uint16_t>((data[4] << 8) | data[3]);
--- tests/drc.test.cpp
+++ tests/drc.test.cpp
@@ -1,1 +1,341 @@
 #include <librmd/drc.hpp>
+
+#include <boost/ut.hpp>
+#include <iostream>
+#include <libhal/can/mock.hpp>
+
+namespace hal::rmd {
+namespace {
+constexpr can::id_t expected_id = 0x140;
+constexpr can::id_t expected_gear_ratio = 6.0f;
+
+template<size_t N>
+constexpr auto prefilled_messages(hal::byte p_command_byte = 0x00)
+{
+  std::array<can::message_t, N> prefilled_expected_messages{};
+  for (auto& message : prefilled_expected_messages) {
+    message.id = expected_id;
+    message.length = 8;
+    message.payload.fill(0x00);
+    message.payload[0] = p_command_byte;
+  }
+  return prefilled_expected_messages;
+};
+}  // namespace
+
+boost::ut::suite drc_test = []() {
+  using namespace boost::ut;
+
+  "drc::create()"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto expected = prefilled_messages<2>();
+    expected[0].payload[0] = value(drc::system::off);
+    expected[1].payload[0] = value(drc::system::running);
+
+    // Exercise
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+
+    // Verify
+    expect(that % 2 == mock_can.spy_send.call_history().size());
+    expect(that % expected[0] == mock_can.spy_send.history<0>(0));
+    expect(that % expected[1] == mock_can.spy_send.history<0>(1));
+  };
+
+  "drc::create() failure"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    mock_can.spy_send.trigger_error_on_call(1);
+
+    constexpr can::message_t expected1{
+      .id = expected_id,
+      .payload = { value(drc::system::off),
+                   0x00,
+                   0x00,
+                   0x00,
+                   0x00,
+                   0x00,
+                   0x00,
+                   0x00 },
+      .length = 8,
+    };
+
+    // Exercise
+    auto driver = drc::create(router, expected_gear_ratio, expected_id);
+
+    // Verify
+    expect(that % 1 == mock_can.spy_send.call_history().size());
+    expect(expected1 == mock_can.spy_send.history<0>(0));
+  };
+
+  "drc::velocity_control()"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+    mock_can.reset();
+    auto expected = prefilled_messages<7>(value(drc::actuate::speed));
+    std::array injected_rpm{
+      0.0_rpm, 10.0_rpm, 10.0_rpm, 123.0_rpm, 0.0_rpm, 1024.0_rpm,
+    };
+
+    using payload_t = decltype(expected[0].payload);
+
+    // Setup: values verified to be correct
+    expected[1].payload = payload_t{
+      0xa2, 0x0, 0x0, 0x0, 0xa0, 0x8c, 0x0, 0x0,
+    };
+    expected[2].payload = payload_t{
+      0xa2, 0x0, 0x0, 0x0, 0xa0, 0x8c, 0x0, 0x0,
+    };
+    expected[3].payload = payload_t{
+      0xa2, 0x0, 0x0, 0x0, 0xb0, 0xc1, 0x6, 0x0,
+    };
+    expected[4].payload = payload_t{
+      0xa2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    };
+    expected[5].payload = payload_t{
+      0xa2, 0x0, 0x0, 0x0, 0x0, 0x40, 0x38, 0x0,
+    };
+
+    // Exercise
+    auto result0 = driver.velocity_control(injected_rpm[0]);
+    auto result1 = driver.velocity_control(injected_rpm[1]);
+    auto result2 = driver.velocity_control(injected_rpm[2]);
+    auto result3 = driver.velocity_control(injected_rpm[3]);
+    auto result4 = driver.velocity_control(injected_rpm[4]);
+    auto result5 = driver.velocity_control(injected_rpm[5]);
+
+    // Verify
+    expect(bool{ result0 });
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(bool{ result3 });
+    expect(bool{ result4 });
+    expect(bool{ result5 });
+
+    expect(that % 6 == mock_can.spy_send.call_history().size());
+    expect(that % expected[0] == mock_can.spy_send.history<0>(0));
+    expect(that % expected[1] == mock_can.spy_send.history<0>(1));
+    expect(that % expected[2] == mock_can.spy_send.history<0>(2));
+    expect(that % expected[3] == mock_can.spy_send.history<0>(3));
+    expect(that % expected[4] == mock_can.spy_send.history<0>(4));
+    expect(that % expected[5] == mock_can.spy_send.history<0>(5));
+  };
+
+  "drc::velocity_control() fails out of bounds"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+    mock_can.reset();
+
+    attempt_all(
+      [&driver]() -> status {
+        // Exercise
+        return driver.velocity_control(1000000.0_rpm);
+      },
+      // Verify
+      [](match<std::errc, std::errc::result_out_of_range>) { expect(true); },
+      []() { expect(false); });
+
+    expect(that % 0 == mock_can.spy_send.call_history().size());
+  };
+
+  "drc::position_control()"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+    mock_can.reset();
+    auto expected = prefilled_messages<6>(value(drc::actuate::position_2));
+    using payload_t = decltype(expected[0].payload);
+
+    // Setup: values verified to be correct
+    expected[0].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0x0, 0x0, 0x0, 0x0,
+    };
+    expected[1].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0x78, 0x69, 0x0, 0x0,
+    };
+    expected[2].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0xf0, 0xd2, 0x0, 0x0,
+    };
+    expected[3].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0x20, 0x1c, 0x0, 0x0,
+    };
+    expected[4].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0xd8, 0xdc, 0xff, 0xff,
+    };
+    expected[5].payload = payload_t{
+      0xa4, 0x0, 0x68, 0x1, 0x40, 0xc6, 0xf9, 0xff,
+    };
+
+    // Exercise
+    auto result0 = driver.position_control(0.0_deg, 10.0_rpm);
+    auto result1 = driver.position_control(45.0_deg, 10.0_rpm);
+    auto result2 = driver.position_control(90.0_deg, 10.0_rpm);
+    auto result3 = driver.position_control(12.0_deg, 10.0_rpm);
+    auto result4 = driver.position_control(-15.0_deg, 10.0_rpm);
+    auto result5 = driver.position_control(-680.0_deg, 10.0_rpm);
+
+    // Verify
+    expect(bool{ result0 });
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(bool{ result3 });
+    expect(bool{ result4 });
+    expect(bool{ result5 });
+
+    expect(that % expected[0] == mock_can.spy_send.history<0>(0));
+    expect(that % expected[1] == mock_can.spy_send.history<0>(1));
+    expect(that % expected[2] == mock_can.spy_send.history<0>(2));
+    expect(that % expected[3] == mock_can.spy_send.history<0>(3));
+    expect(that % expected[4] == mock_can.spy_send.history<0>(4));
+    expect(that % expected[5] == mock_can.spy_send.history<0>(5));
+  };
+
+  "drc::position_control() fails"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+
+    // Exercise
+    attempt_all(
+      [&driver]() -> status {
+        return driver.position_control(1000000.0_deg, 10.0_rpm);
+      },
+      // Verify
+      [](match<std::errc, std::errc::result_out_of_range>) { expect(true); },
+      []() { expect(false); });
+
+    attempt_all(
+      [&driver]() -> status {
+        // Exercise
+        return driver.position_control(0.0_deg, 1000000.0_rpm);
+      },
+      // Verify
+      [](match<std::errc, std::errc::result_out_of_range>) { expect(true); },
+      []() { expect(false); });
+  };
+
+  "drc::feedback_request()"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+    mock_can.reset();
+
+    auto expected = prefilled_messages<8>();
+
+    expected[0].payload[0] = hal::value(drc::read::pid_data);
+    expected[1].payload[0] = hal::value(drc::read::acceleration_data);
+    expected[2].payload[0] = hal::value(drc::read::encoder_data);
+    expected[3].payload[0] = hal::value(drc::read::multi_turns_angle);
+    expected[4].payload[0] = hal::value(drc::read::single_circle_angle);
+    expected[5].payload[0] = hal::value(drc::read::status_1_and_error_flags);
+    expected[6].payload[0] = hal::value(drc::read::status_2);
+    expected[7].payload[0] = hal::value(drc::read::status_3);
+
+    // Exercise
+    auto result0 = driver.feedback_request(drc::read::pid_data);
+    auto result1 = driver.feedback_request(drc::read::acceleration_data);
+    auto result2 = driver.feedback_request(drc::read::encoder_data);
+    auto result3 = driver.feedback_request(drc::read::multi_turns_angle);
+    auto result4 = driver.feedback_request(drc::read::single_circle_angle);
+    auto result5 = driver.feedback_request(drc::read::status_1_and_error_flags);
+    auto result6 = driver.feedback_request(drc::read::status_2);
+    auto result7 = driver.feedback_request(drc::read::status_3);
+
+    // Verify
+    expect(bool{ result0 });
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(bool{ result3 });
+    expect(bool{ result4 });
+    expect(bool{ result5 });
+    expect(bool{ result6 });
+    expect(bool{ result7 });
+
+    expect(that % 8 == mock_can.spy_send.call_history().size());
+    expect(that % expected[0] == mock_can.spy_send.history<0>(0));
+    expect(that % expected[1] == mock_can.spy_send.history<0>(1));
+    expect(that % expected[2] == mock_can.spy_send.history<0>(2));
+    expect(that % expected[3] == mock_can.spy_send.history<0>(3));
+    expect(that % expected[4] == mock_can.spy_send.history<0>(4));
+    expect(that % expected[5] == mock_can.spy_send.history<0>(5));
+    expect(that % expected[6] == mock_can.spy_send.history<0>(6));
+    expect(that % expected[7] == mock_can.spy_send.history<0>(7));
+  };
+
+  "drc::feedback_request() fails"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+
+    mock_can.spy_send.reset();
+    mock_can.spy_send.trigger_error_on_call(1);
+
+    // Exercise
+    auto result = driver.feedback_request(drc::read::pid_data);
+
+    // Verify
+    expect(!bool{ result });
+    expect(that % 1 == mock_can.spy_send.call_history().size());
+  };
+
+  "drc::system_control()"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+    mock_can.reset();
+
+    auto expected = prefilled_messages<4>();
+
+    expected[0].payload[0] = hal::value(drc::system::clear_error_flag);
+    expected[1].payload[0] = hal::value(drc::system::off);
+    expected[2].payload[0] = hal::value(drc::system::stop);
+    expected[3].payload[0] = hal::value(drc::system::running);
+
+    // Exercise
+    auto result0 = driver.system_control(drc::system::clear_error_flag);
+    auto result1 = driver.system_control(drc::system::off);
+    auto result2 = driver.system_control(drc::system::stop);
+    auto result3 = driver.system_control(drc::system::running);
+
+    // Verify
+    expect(bool{ result0 });
+    expect(bool{ result1 });
+    expect(bool{ result2 });
+    expect(bool{ result3 });
+
+    expect(that % 4 == mock_can.spy_send.call_history().size());
+    expect(that % expected[0] == mock_can.spy_send.history<0>(0));
+    expect(that % expected[1] == mock_can.spy_send.history<0>(1));
+    expect(that % expected[2] == mock_can.spy_send.history<0>(2));
+    expect(that % expected[3] == mock_can.spy_send.history<0>(3));
+  };
+
+  "drc::system_control() fails"_test = []() {
+    // Setup
+    mock::can mock_can;
+    auto router = can_router::create(mock_can).value();
+    auto driver = drc::create(router, expected_gear_ratio, expected_id).value();
+
+    mock_can.spy_send.reset();
+    mock_can.spy_send.trigger_error_on_call(1);
+
+    // Exercise
+    auto result = driver.system_control(drc::system::clear_error_flag);
+
+    // Verify
+    expect(!bool{ result });
+    expect(that % 1 == mock_can.spy_send.call_history().size());
+  };
+};
+}  // namespace hal::rmd
