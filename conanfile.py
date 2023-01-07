bd1c5b60ffe42a3a111ad98d3238269e7841ad61
MaliaLabor <malia.labor@hotmail.com>
2023-01-07 13:31:27 -0800

    :sparkle: hal::callback + interface API updates

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ required_conan_version = ">=1.50.0"
 
 class LibrmdConan(ConanFile):
     name = "librmd"
-    version = "0.3.1"
+    version = "0.3.2"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/librmd"
@@ -38,8 +38,8 @@ class LibrmdConan(ConanFile):
         }
 
     def requirements(self):
-        self.requires("libhal/0.3.1@")
-        self.requires("libhal-util/0.3.1@")
+        self.requires("libhal/0.3.2@")
+        self.requires("libhal-util/0.3.3@")
 
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
