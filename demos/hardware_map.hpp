19d40a868e557763ab7282bac70857a129371e59
Khalil Estell <kammce@google.com>
2023-07-09 06:31:28 -0700

    :pushpin: Fix mc_x demo

--- demos/hardware_map.hpp
+++ demos/hardware_map.hpp
@@ -30,4 +30,5 @@ struct hardware_map
 // Application function must be implemented by one of the compilation units
 // (.cpp) files.
 hal::status application(hardware_map& p_map);
-hal::result<hardware_map> initialize_target();
+hal::status initialize_processor();
+hal::result<hardware_map> initialize_platform();
