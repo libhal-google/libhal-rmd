f8a99da57c01ac1bd34f6b884056e91bfb795e45
Khalil Estell <kammce@google.com>
2023-08-06 11:09:36 -0700

    :arrow_down: Bring version back to 3.0.0

--- demos/conanfile.py
+++ demos/conanfile.py
@@ -28,7 +28,7 @@ class demos(ConanFile):
     def requirements(self):
         if str(self.options.platform).startswith("lpc40"):
             self.requires("libhal-lpc40/[^2.1.1]")
-        self.requires("libhal-rmd/3.0.1")
+        self.requires("libhal-rmd/3.0.0")
 
     def layout(self):
         platform_directory = "build/" + str(self.options.platform)
